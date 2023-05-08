f6cbf4fb94d787ceb50340c718725083209d9cfa
unknown <k.kosimi@yandex.com>
2023-05-08 19:53:35 +0500

    Initial commit

---
+++ Notes.Backend/Notes.Application/Common/Mappings/IMapWith.cs
@@ -0,0 +1,10 @@
+﻿using AutoMapper;
+
+namespace Notes.Application.Common.Mappings
+{
+    public interface IMapWith<T>
+    {
+        void Mappings(Profile profile) =>
+            profile.CreateMap(typeof(T), GetType());
+    }
+}
---
+++ Notes.Backend/Notes.Persistence/DbInitializer.cs
@@ -0,0 +1,12 @@
+﻿using Notes.Persistence;
+
+namespace Notes.Persistence
+{
+    public class DbInitializer
+    {
+        public static void Initialize(NotesDbContext context)
+        {
+            context.Database.EnsureCreated();
+        }
+    }
+}
