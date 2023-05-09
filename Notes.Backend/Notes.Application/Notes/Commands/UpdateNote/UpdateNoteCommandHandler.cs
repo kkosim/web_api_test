0dc25b2b548dca6908c9aad8ebd8554dc90331bf
unknown <k.kosimi@yandex.com>
2023-05-09 15:38:39 +0500

    updateNoteHandler

---
+++ Notes.Backend/Notes.Application/Notes/Commands/UpdateNote/UpdateNoteCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Notes.Application.Interfaces;
+using Notes.Application.Common.Exceptions;
+using Notes.Domain;
+
+namespace Notes.Application.Notes.Commands.UpdateNote
+{
+    public class UpdateNoteCommandHandler
+        : IRequestHandler<UpdateNoteCommand>
+    {
+        private readonly INotesDbContext _dbcontext;
+        public UpdateNoteCommandHandler(INotesDbContext dbcontext) =>
+            _dbcontext = dbcontext;
+        public async Task<Unit> Handle(UpdateNoteCommand request,
+            CancellationToken cancellationToken)
+        {
+            var entity =
+                await _dbcontext.Notes.FirstOrDefaultAsync(note =>
+                note.Id == request.Id, cancellationToken);
+
+            if (entity == null || entity.UserId != request.UserId)
+            {
+                throw new NotFoundException(nameof(Note), request.Id);
+            }
+            entity.Details = request.Details;
+            entity.Title = request.Title;
+            entity.EditDate = DateTime.Now;
+
+            await _dbcontext.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+
+        Task IRequestHandler<UpdateNoteCommand>.Handle(UpdateNoteCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
