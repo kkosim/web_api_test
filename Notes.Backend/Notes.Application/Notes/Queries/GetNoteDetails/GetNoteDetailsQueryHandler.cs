4fa1fc662303313ed29bf3d4dc903eb3021b1083
unknown <k.kosimi@yandex.com>
2023-05-09 19:23:31 +0500

    Queries handler

---
+++ Notes.Backend/Notes.Application/Notes/Queries/GetNoteDetails/GetNoteDetailsQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using System;
+using System.Threading.Tasks;
+using System.Threading;
+using Notes.Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using Notes.Application.Common.Exceptions;
+using Notes.Domain;
+
+namespace Notes.Application.Notes.Queries.GetNoteDetails
+{
+    public class GetNoteDetailsQueryHandler
+        :IRequestHandler<GetNoteDetailsQuery, NoteDetailsVm>
+    {
+        private readonly INotesDbContext _dbcontext;
+        private readonly IMapper _mapper;
+        public GetNoteDetailsQueryHandler(INotesDbContext dbcontext,
+            IMapper mapper) => (_dbcontext, _mapper) = (dbcontext, mapper);          
+
+        public async Task<NoteDetailsVm> Handle(GetNoteDetailsQuery request,
+            CancellationToken cancellationToken)
+        {
+            var entity = await _dbcontext.Notes
+                .FirstOrDefaultAsync(note =>
+                note.Id == request.Id, cancellationToken);
+
+            if (entity == null || entity.Id != request.Id)
+            {
+                throw new NotFoundException(nameof(Note), request.Id);
+            }
+            return _mapper.Map<NoteDetailsVm>(entity);
+        }
+    }
+}
