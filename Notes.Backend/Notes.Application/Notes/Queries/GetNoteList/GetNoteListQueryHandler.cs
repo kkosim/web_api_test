2b6c38f78490d80981cd1a151b36a43c5329cffa
unknown <k.kosimi@yandex.com>
2023-05-09 20:39:28 +0500

    GetNoteList update

---
+++ Notes.Backend/Notes.Application/Notes/Queries/GetNoteList/GetNoteListQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using MediatR;
+using Notes.Application.Interfaces;
+
+namespace Notes.Application.Notes.Queries.GetNoteList
+{
+    public class GetNoteListQueryHandler
+        :IRequestHandler<GetNoteListQuery, NoteListVm>
+    {
+        private readonly INotesDbContext _dbcontext;
+        private readonly IMapper _mapper;
+
+        public GetNoteListQueryHandler(INotesDbContext dbcontext, IMapper mapper)
+        {
+            _dbcontext = dbcontext;
+            _mapper = mapper;
+        }
+        public async Task<NoteListVm> Handle(GetNoteListQuery request,
+            CancellationToken cancellationToken)
+        {
+            var notesQuery = await _dbcontext.Notes
+                .Where(note => note.UserId == request.UserId)
+                .ProjectTo<NoteLookupDto>(_mapper.ConfigurationProvider)
+                .ToListAsync(cancellationToken);
+            return new NoteListVm { Notes = notesQuery };
+        }
+    }
+}
