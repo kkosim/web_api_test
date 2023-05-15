b7c2e6a5c2e9d7a112ed7dfa4337b84f44f789ba
kkosim <k.kosimi@yandex.com>
2023-05-15 18:54:43 +0500

    controllers added

---
+++ Notes.Backend/Notes.WebApi/Controllers/NoteController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Notes.Application.Notes.Queries.GetNoteList;
+using Notes.Application.Notes.Queries.GetNoteDetails;
+
+namespace Notes.WebApi.Controllers
+{
+    public class NoteController : BaseController
+    {
+        [HttpGet]
+        public async Task<ActionResult<NoteListVm>> GetAll()
+        {
+            var querry = new GetNoteListQuery
+            {
+                UserId = UserId
+            };
+
+            var vm = await Mediator.Send(querry);
+            return Ok(vm);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<NoteDetailsVm>> Get(Guid id)
+        {
+            var querry = new GetNoteDetailsQuery
+            { 
+                UserId = UserId,
+                Id = id 
+            };
+            var vm = await Mediator.Send(querry);
+            return Ok(vm);
+        }
+    }
+}
