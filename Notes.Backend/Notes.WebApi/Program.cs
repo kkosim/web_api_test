11c1481f501c3c00e8056678b2281be71d935a8e
kkosim <k.kosimi@yandex.com>
2023-05-15 15:53:42 +0500

    commit

--- Notes.Backend/Notes.WebApi/Program.cs
+++ Notes.Backend/Notes.WebApi/Program.cs
@@ -1,6 +1,37 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using System;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Notes.Persistence;
 
-app.MapGet("/", () => "Hello World!");
+namespace Notes.WebApi
+{
+    public class Program
+    {
+        public static void Main(string[] args) 
+        {
+            var host = CreateHostBuilder(args).Build();
+            
+            using (var scope = host.Services.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+                try
+                {
+                    var context = serviceProvider.GetRequiredService<NotesDbContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception exciption)
+                {
 
-app.Run();
+                }
+            }
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+    }
+}
